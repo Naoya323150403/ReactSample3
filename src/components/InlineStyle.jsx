9ebd00d68ca162468ab1b930dddb23495a7670d9
Naoya Teraoka <59727299+Naoya323150403@users.noreply.github.com>
2022-12-16 00:56:14 +0900

    2022/12/15

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,25 @@
+export const InlineStyle = () => {
+  const containerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    justifyContent: "space-around"
+  };
+  const titleStyle = {
+    margin: "0",
+    color: "#3d84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "None",
+    padding: "8px"
+  };
+
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}> --Inline Styles-- </p>
+      <button style={buttonStyle}> FIGHT! </button>
+    </div>
+  );
+};
